d270d73911d9b96732b59aea81772202607aaaba
Rin Shimizu <52402142+rei-smz@users.noreply.github.com>
2024-05-03 13:14:34 -0400

    Delete config.py

--- video-transcode/handler.py
+++ video-transcode/handler.py
@@ -10,6 +10,10 @@ import parsl
 from parsl import bash_app
 from config import transcode_config_local
 from parsl.data_provider.files import File
+from parsl.config import Config
+from parsl.executors import HighThroughputExecutor
+from parsl.providers import LocalProvider
+from parsl.channels import LocalChannel
 
 def get_secret(key):
     with open("/var/openfaas/secrets/{}".format(key)) as f:
@@ -60,6 +64,23 @@ def handle(event, context):
         "body": "Method not allowed"
       }
     
+    transcode_config_local = Config(
+    executors=[
+        HighThroughputExecutor(
+            label="htex_Local",
+            worker_debug=True,
+            # cores_per_worker=1,
+            # storage_access=[HTTPInTaskStaging()],
+            provider=LocalProvider(
+                channel=LocalChannel(),
+                init_blocks=1,
+                max_blocks=10,
+            ),
+        )
+    ],
+    strategy=None,
+)
+
     with parsl.load(transcode_config_local):
         body = json.loads(event.body.decode('utf-8'))
         
